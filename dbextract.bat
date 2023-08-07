11081dcd3a09896d242db8f39d7ba25431674cd9
V <44802344+V-Cyberpunk@users.noreply.github.com>
2023-08-07 20:20:42 +0200

    Add files via upload

--- dbextract.bat
+++ dbextract.bat
@@ -4,8 +4,10 @@ start cmd /c "mysqld.exe --defaults-file=my.ini --console"
 timeout /t 5
 mysqldump -u root -padmin hotfixes > hotfixes.sql
 mysqldump -u root -padmin world > world.sql
+mysqldump -u root -padmin --no-data --no-create-info --no-create-db --no-set-names --skip-comments --skip-dump-date --skip-opt --triggers characters > characters_trigger.sql
 move hotfixes.sql %~dp0
 move world.sql %~dp0
+move characters_trigger.sql %~dp0
 taskkill /F /IM mysqld.exe
 cd %~dp0
 rmdir /S /Q %~dp0PreCompiledCore
